484f1e9a5d5effee08e6bbb358ba011b2a71ecc9
John McCambridge <47507224+johnmccambridge7@users.noreply.github.com>
2020-06-28 20:35:53 -0700

    Added EW form

--- src/pages/SurveyPage/index.js
+++ src/pages/SurveyPage/index.js
@@ -51,7 +51,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "proof_title",
-            "html": "<h2>Please provide proof that you are an essential worker. *</h2><br>\n<b>Examples:</b> a screenshot of an email from your manager (with a date and name), your last paycheck, screenshots from an employee app (your name must be included in the picture), or any other documentation that will help us verify your status.\n<br><br>\nWe must have proof in order to match you with a donor."
+            "html": "<h5>Please provide proof that you are an essential worker. *</h5><br>\n<b>Examples:</b> a screenshot of an email from your manager (with a date and name), your last paycheck, screenshots from an employee app (your name must be included in the picture), or any other documentation that will help us verify your status.\n<br><br>\nWe must have proof in order to match you with a donor."
            },
            {
             "type": "file",
@@ -69,7 +69,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "notice",
-            "html": "<h2>The following questions ask for your contact information so that we can facilitate the match.</h2>"
+            "html": "<h5>The following questions ask for your contact information so that we can facilitate the match.</h5>"
            }
           ]
          },
@@ -147,7 +147,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "notice_2",
-            "html": "<h4>* Optional - so that we can text you in case we can't reach you via email.</h4>"
+            "html": "<h5>* Optional - so that we can text you in case we can't reach you via email.</h5>"
            }
           ]
          },
@@ -194,7 +194,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "notice_3",
-            "html": "<h2>If you selected Kids' Activities, please specify an age range and any other helpful information\n<br><br>\nPersonal Hygiene Products: please specify what type of product and any preferred brands\n<br><br>\nGift Cards: please specify what category and stores would be most helpful for you to receive</h2>\n<p>Please skip if not applicable</p>"
+            "html": "<h5>If you selected Kids' Activities, please specify an age range and any other helpful information\n<br><br>\nPersonal Hygiene Products: please specify what type of product and any preferred brands\n<br><br>\nGift Cards: please specify what category and stores would be most helpful for you to receive</h5>\n\n<p>Please skip if not applicable</p>"
            },
            {
             "type": "text",
@@ -209,7 +209,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "question16",
-            "html": "<h2>Please provide a description of your circumstances so we can better understand what you might need.</h2><br>\n<h3>Ex: Are you a grocery shopper who needs help with gas? Are you a single mother who needs activities for her kids?\n<br>\nFeel free to tell us anything about your situation and the items that you need so that we can better help you.</h3>"
+            "html": "<h5>Please provide a description of your circumstances so we can better understand what you might need.</h5><br>\n<h5>Ex: Are you a grocery shopper who needs help with gas? Are you a single mother who needs activities for her kids?\n<br>\nFeel free to tell us anything about your situation and the items that you need so that we can better help you.</h5>"
            },
            {
             "type": "text",
@@ -225,7 +225,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "question20",
-            "html": "<h2>I consent to Give Essential to sharing the information I have provided about my circumstances with my donor.</h2><br>\n<h3>This will help them get a better idea of what types of gifts to send. Company names will be removed, and donors have pledged to only use the information they're given for the sole purpose of sending a gift. \n<br><br>\nIn our experience, donors are more likely to send a package when we are allowed to share your story with them.</h3>"
+            "html": "<h5>I consent to Give Essential to sharing the information I have provided about my circumstances with my donor.</h5><br>\n<h5>This will help them get a better idea of what types of gifts to send. Company names will be removed, and donors have pledged to only use the information they're given for the sole purpose of sending a gift. \n<br><br>\nIn our experience, donors are more likely to send a package when we are allowed to share your story with them.</h5>"
            },
            {
             "type": "radiogroup",
@@ -251,7 +251,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "question19",
-            "html": "<h2>I consent to Give Essential anonymously sharing my story on Give Essential media.</h2><br>\n<h3>We want to share stories to help reach donors. <br><br> Stories will be completely anonymous on our public media; all personal identification information will be removed (name, company name, address, etc).</h3>"
+            "html": "<h5>I consent to Give Essential anonymously sharing my story on Give Essential media.</h5><br>\n<h3>We want to share stories to help reach donors. <br><br> Stories will be completely anonymous on our public media; all personal identification information will be removed (name, company name, address, etc).</h3>"
            },
            {
             "type": "radiogroup",
@@ -299,7 +299,7 @@ class SurveyPage extends React.Component {
            {
             "type": "html",
             "name": "question25",
-            "html": "<h2>I understand that my donor will receive my first and last name, email address, mailing address, and if I consented, the story that I shared. I understand that these items are publicly sourced from un-vetted individuals, including individuals who themselves may be carrying COVID-19, and take full responsibility for the quality, efficacy, and safety of these donations and their use, and full responsibility for the information I have shared.\n<br>\n<br>\nGive Essential connects people with publicly crowd-sourced donation items to essential workers during this time of critical shortage and inaccessibility. All items are being donated and are provided without any warranty, guarantee, or certification of fitness for purpose. Nothing in this website or in the course of providing these donations establishes a legal obligation or promise on Give Essential's behalf.This question is required.</h2>"
+            "html": "<h5>I understand that my donor will receive my first and last name, email address, mailing address, and if I consented, the story that I shared. I understand that these items are publicly sourced from un-vetted individuals, including individuals who themselves may be carrying COVID-19, and take full responsibility for the quality, efficacy, and safety of these donations and their use, and full responsibility for the information I have shared.\n<br>\n<br>\nGive Essential connects people with publicly crowd-sourced donation items to essential workers during this time of critical shortage and inaccessibility. All items are being donated and are provided without any warranty, guarantee, or certification of fitness for purpose. Nothing in this website or in the course of providing these donations establishes a legal obligation or promise on Give Essential's behalf.This question is required.</h5>"
            },
            {
             "type": "radiogroup",
@@ -334,7 +334,7 @@ class SurveyPage extends React.Component {
         return (
             <Container>
                 <DonorForm>
-                    <h1>Donor Form</h1>
+                    <h1>Essential Worker Form</h1>
                     <Survey.Survey json={ this.json } />
                 </DonorForm>
             </Container>
