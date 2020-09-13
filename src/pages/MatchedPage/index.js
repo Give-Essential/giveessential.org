5cee27a4d03dbcc2683614bb5e2fefde051f0e19
Tracy Lihui Zhang <tracylihuizhang@Tracys-MacBook-Pro-2.local>
2020-09-13 10:18:25 -0400

    moving to named export

--- src/pages/MatchedPage/index.js
+++ src/pages/MatchedPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import { Link } from "react-router-dom";
 
@@ -25,6 +25,22 @@ import gift from "./Icons/gift.svg";
 import tracking from "./Icons/tracking.svg";
 import hand from "./Icons/hand.svg";
 
+import  WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat:thin', 'open-serif'],
+  }
+});
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
 const nextStepsList = [
   [
     mail,
@@ -41,43 +57,70 @@ const nextStepsList = [
   ],
 ];
 
-export default function MatchedPage() {
-  return (
-    <BackgroundContainer>
-        <HeaderText>
-          You’ve been matched!
-        </HeaderText>
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
-      <NextSteps>Next Steps</NextSteps>
-      <div style={{ padding: "0% 12%" }}>
-        {nextStepsList.map((e, i) => (
-          // <RowFlex style={{ display: "flex", margin: "2% 6%" }}>
-          <Row style={{ margin: "3% 5%" }} classname="my-auto" key={i}>
-           <RowFlex>
-              <Col xs="2">
-                <Dot color={"#8CC9BA"}>
-                  <DotText>{i + 1}</DotText>
-                </Dot>
-              </Col>
-              <Col xs="2">
-                <StyledIcon src={e[0]} alt="Logo for corresponding Icon" />
-              </Col>
-            </RowFlex>
-            <Col>
-              <ListText>{e[1]}</ListText>
-            </Col>
-          </Row>
-        ))}
-      </div>
-      <Donation>Thank you for your donation!</Donation>
-      <Container className="my-auto" style={{ textAlign: "center" }}>
-        <Link to="/">
-          <Button primary>Return to homepage</Button>
-        </Link>
-        <Link>
-          <Button>Refer a friend</Button>
-        </Link>
-      </Container>
-    </BackgroundContainer>
-  );
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+const {height, width} = useWindowDimensions();
+
+// export default function MatchedPage() {
+//   if (width < 420) {
+//     return (
+//         <div>Hello</div>
+//     )
+//   } else {
+//     return (
+//         <BackgroundContainer>
+//           <HeaderText>
+//             You’ve been matched!
+//           </HeaderText>
+//
+//           <NextSteps>Next Steps</NextSteps>
+//           <div style={{ padding: "0% 12%" }}>
+//             {nextStepsList.map((e, i) => (
+//                 // <RowFlex style={{ display: "flex", margin: "2% 6%" }}>
+//                 <Row style={{ margin: "3% 5%" }} classname="my-auto" key={i}>
+//                   <RowFlex>
+//                     <Col xs="2">
+//                       <Dot color={"#8CC9BA"}>
+//                         <DotText>{i + 1}</DotText>
+//                       </Dot>
+//                     </Col>
+//                     <Col xs="2">
+//                       <StyledIcon src={e[0]} alt="Logo for corresponding Icon" />
+//                     </Col>
+//                   </RowFlex>
+//                   <Col>
+//                     <ListText>{e[1]}</ListText>
+//                   </Col>
+//                 </Row>
+//             ))}
+//           </div>
+//           <Donation>Thank you for your donation!</Donation>
+//           <Container className="my-auto" style={{ textAlign: "center" }}>
+//             <Link to="/">
+//               <Button primary>Return to homepage</Button>
+//             </Link>
+//             <Link>
+//               <Button>Refer a friend</Button>
+//             </Link>
+//           </Container>
+//         </BackgroundContainer>
+//     );
+//   }
+//
+// }
+
+const renderStep = () => {
+
 }
